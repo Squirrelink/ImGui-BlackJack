19f3b859835f6afd26e0e040363e5d1fb3e79c48
John Wachala <jwacha4@illinois.edu>
2020-05-03 01:32:51 +0200

    added private section to my_app

--- apps/my_app.h
+++ apps/my_app.h
@@ -13,26 +13,8 @@
 namespace myapp {
 
 class MyApp : public cinder::app::App {
- public:
-  MyApp();
-  void setup() override;
-  void update() override;
-  void draw() override;
-  void keyDown(cinder::app::KeyEvent) override;
-  void MenuButton();
-  void LoadImages();
-  void LoadSounds();
-  
-  
-  cinder::gl::Texture2dRef background_Texture;
-  cinder::gl::Texture2dRef deck_Texture;
-  cinder::gl::Texture2dRef one_chip_Texture;
-  cinder::gl::Texture2dRef hundred_chip_Texture;
-  cinder::gl::Texture2dRef thousand_chip_Texture;
-  cinder::gl::Texture2dRef ten_chip_Texture;
-  cinder::gl::Texture2dRef max_chip_Texture;
-  cinder::gl::Texture2dRef card_back_Texture;
-  
+ private:
+  mylibrary::Engine engine;
   void DrawGameState();
   void DrawGameButtons();
   void DrawBetButtons();
@@ -42,7 +24,6 @@ class MyApp : public cinder::app::App {
   static void DrawTie();
   void DrawScore();
   void DrawDealerScore();
-  mylibrary::Engine engine;
   void DrawPlayerCards();
   void DrawDealerCards();
   void DrawInitialDealerCards();
@@ -54,6 +35,9 @@ class MyApp : public cinder::app::App {
   void UpdateScore();
   void DrawEndGameText();
   void DrawExitButton();
+  void MenuButton();
+  void LoadImages();
+  void LoadSounds();
   ci::audio::VoiceRef mVoice;
   ci::audio::VoiceRef shuffel_sound;
   ci::audio::VoiceRef single_shuffel_sound;
@@ -61,6 +45,26 @@ class MyApp : public cinder::app::App {
   ci::audio::VoiceRef single_chip_sound;
   ci::audio::VoiceRef multiple_chip_sound;
   cinder::gl::Texture2dRef GetCardTexture(int value, int color);
+  cinder::gl::Texture2dRef background_Texture;
+  cinder::gl::Texture2dRef deck_Texture;
+  cinder::gl::Texture2dRef one_chip_Texture;
+  cinder::gl::Texture2dRef hundred_chip_Texture;
+  cinder::gl::Texture2dRef thousand_chip_Texture;
+  cinder::gl::Texture2dRef ten_chip_Texture;
+  cinder::gl::Texture2dRef max_chip_Texture;
+  cinder::gl::Texture2dRef card_back_Texture;
+ public:
+  MyApp();
+  void setup() override;
+  void update() override;
+  void draw() override;
+  void keyDown(cinder::app::KeyEvent) override;
+  
+  
+  
+  
+  
+  
 };
 
 }  // namespace myapp
